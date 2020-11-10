7d55f7fc2094fb7794fca161b7304ac0d0c4218f
paolo <paolo@LAPTOP-L1BQ14BO>
2020-11-10 18:47:12 +0100

    ciao a tutti

---
+++ src/main/java/NewClass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author paolo
+ */
+public class NewClass {
+    double i=0;
+    while(true){
+            System.out.println("ehi ciao"+ i);
+            i=i*i;
+            i++;
+}
+}
